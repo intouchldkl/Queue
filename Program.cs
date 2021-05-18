40ad5289171a5d8c5873fb92059888a536796b58
ADMINS <ADMINS@DESKTOP-QFR4487>
2021-05-18 15:06:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQ = new Queue();
+            myQ.Enqueue("Allen");
+            myQ.Enqueue("Barry");
+            myQ.Enqueue("Chris");
+            myQ.Enqueue("Dave");
+            Console.WriteLine(myQ.Dequeue().name);
+            Console.WriteLine(myQ.Dequeue().name);
+            Console.WriteLine(myQ.Dequeue().name);
+            Console.WriteLine(myQ.Dequeue().name);
+            Console.WriteLine(myQ.Dequeue().name);
+            myQ.Enqueue("Tony");
+            Console.WriteLine(myQ.Dequeue().name);
+            Console.WriteLine(myQ.Dequeue().name);
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class Queue
+    {
+        public Node front = null;
+        public Node back = null;
+
+        public Queue()
+        {
+
+        }
+
+        public void Enqueue(string name)
+        {
+            Node newNode = new Node(name, null);
+            if(front == null)
+            {
+                front = newNode;
+                
+            }
+            else
+            {
+                back.next = newNode;
+                
+            }
+            back = newNode;
+                    
+        }
+        public Node Dequeue()
+        {
+            if(front == null)
+            {
+                return new Node("Queue is empty", null);
+            }
+            Node returnNode = front;
+            front = front.next;
+            return returnNode;
+        }
+    }
+}
